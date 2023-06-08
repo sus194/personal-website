29b9a0349a0cc872b47532ae08ce7761dc3ad422
sus194 <spa194@sfu.ca>
2023-06-08 00:22:08 -0700

    added fire animation

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -35,23 +35,28 @@ export default function Intro() {
     };
   }, []);
 
+  
+
   return (
+    
     <div
       ref={introRef}
       className={`intro-container ${fadeIn ? 'fade-in' : ''}`}
     >
-      <h1 className="intro-title">About</h1>
-      <div
+    
+
+        <h1 className="intro-title">About</h1>
+        <div
           className={`intro-image ${slideIn ? 'slide-in' : ''}`}
         >
-        <Image src="/imgs/photo-2.png" alt="My Image" width={300} height={300} />
-        
-      </div>
-      <div className="intro-description">
+          <Image src="/imgs/photo-2.png" alt="My Image" width={300} height={300} />
+
+        </div>
+        <div className="intro-description">
           <h3>Description</h3>
+        </div>
+
+
       </div>
-     
-      
-    </div>
   );
 }
