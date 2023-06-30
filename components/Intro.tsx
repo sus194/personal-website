f113d7f9618a3618de34f5cf167bd4bb5222d5a1
sus194 <spa194@sfu.ca>
2023-06-29 20:11:05 -0700

    the card animation for the intro page works

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -8,74 +8,76 @@ export default function Intro() {
   const [fadeIn, setFadeIn] = useState(false);
   const [slideIn, setSlideIn] = useState(false);
 
-  useEffect(() => {
-    const handleScroll = (entries: IntersectionObserverEntry[]) => {
-      const introElement = introRef.current;
-      if (introElement) {
-        const [entry] = entries;
-        if (entry.isIntersecting) {
-          setFadeIn(true);
-          setSlideIn(true);
-        }
-      }
-    };
-
-    const observer = new IntersectionObserver(handleScroll, {
-      rootMargin: '0px',
-      threshold: 0.25,
-    });
-
-    if (introRef.current) {
-      observer.observe(introRef.current);
-    }
-
-    return () => {
-      if (introRef.current) {
-        observer.unobserve(introRef.current);
-      }
-    };
-  }, []);
+  
 
   const [isFlipped, setIsFlipped] = useState(false);
+  
+  const [hoverEffect, setHoverEffect] = useState(false);
+
+
+  const handleCardHover = () => {
+    setHoverEffect(true);
+  };
 
   const handleCardClick = () => {
     setIsFlipped(!isFlipped);
+    setFadeIn(true)
+    setSlideIn(true);
+   
   };
 
   
 
   return (
-    <>
-    
     <div
-      ref={introRef}
-      className={`intro-container ${fadeIn ? 'fade-in' : ''}`}
-    >
-
-      <div className="background-image"></div>
-      <h1 className="intro-title">About</h1>
       
-      <div className='intro-description'>
-     
-        <div
-          className={`intro-image ${slideIn ? 'slide-in' : ''}`}
-        >
-          <Image src="/imgs/photo-2.png" alt="My Image" width={300} height={300} />
-
-        </div>
-        <div className="card-container">
-          <ReactCardFlip flipDirection="horizontal" isFlipped={isFlipped}>
-            <div className="card-content" onClick={handleCardClick}>
-              <img src="/imgs/jokercard-front.png" alt="Joker Card front" className="card-front" />
-            </div>
-            <div className="card-content" onClick={handleCardClick}>
-              <img src="/imgs/jokercard-back.png" alt="Joker Card Back" className="card-back" />
-            </div>
-          </ReactCardFlip>
-        </div>
+    >
+      
+        <div >
+        <ReactCardFlip containerClassName = {`card-container ${fadeIn?'fade-out':''}`} flipDirection="horizontal" isFlipped={isFlipped}>
+          <div className={`card-content ${hoverEffect ? `hover-effect ${isFlipped?'flipped':''}` : 'not-hover-effect'}`} onClick={handleCardClick} onMouseEnter={handleCardHover}
+              onMouseLeave={() => setHoverEffect(false)}
+            >
+            <img src="/imgs/jokercard-front.png" alt="Joker Card front" className="card-front" />
+          </div>
+          <div className="card-content" onClick={handleCardClick}>
+            <img src="/imgs/jokercard-back.png" alt="Joker Card Back" className="card-back" />
+          </div>
+        </ReactCardFlip>
       </div>
+        
+       
+
+          
+          <div
+          ref={introRef}
+          className={`intro-container ${fadeIn ? 'fade-in' : ''}`}
+          >
+          
+          <h1 className="intro-title">About</h1>
+          
+          <div className='intro-description'>
+
+              <div
+                className={`intro-image ${slideIn ? 'slide-in' : ''}`}
+              >
+                <Image src="/imgs/photo-2.png" alt="My Image" width={300} height={300} />
+                
+              </div>
+
+              <div className='intro'>
+                  <p>hellgvghcgcycyhcytcycsdssdfsfsvdsfvsdvsvvfdvsvsfo</p>
+              </div>
+
+          </div>
+        </div>
+           
+         
+      
+      
+      
 
 
-    </div></>
+    </div>
   );
 }
