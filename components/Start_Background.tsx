760590834c7abe8df1e68510bef587a00e09211a
sus194 <spa194@sfu.ca>
2023-07-07 23:21:06 -0700

    added a particles

--- components/Start_Background.tsx
+++ components/Start_Background.tsx
@@ -27,11 +27,20 @@ export const Start_Background = () => {
             const y = particlePositions.getY(i);
             const z = particlePositions.getZ(i);
             
-            const waveOffsety = Math.sin(elapsedTime + (x * 0.09)) * 0.1;
-            const waveOffset = Math.sin(elapsedTime + (y * 0.09)) * 0.1; // Adjust the amplitude and frequency as desired
-            const newX = (x + waveOffset); // Adjust the range as desired
-            const newY = (y + waveOffsety);
-            particlePositions.setXYZ(i, newX, y, z);
+            const waveOffset = Math.sin(elapsedTime* 0.2 + (x )) * 0.01;
+            const waveOffsetz = Math.sin(elapsedTime* 0.5 + (y )) * 0.01; // Adjust the amplitude and frequency as desired
+            const direction = new THREE.Vector2(1, 1);
+            let newZ = (z + waveOffsetz); // Adjust the range as desired
+
+            
+            
+            let newY = (y +  direction.y *waveOffset);
+            
+            if (Math.abs(newZ) > 1) {
+                // Adjust the position to stay within the visible area
+                newZ = Math.sign(newZ) * 1; // Adjust maxDistance as needed
+            }
+            particlePositions.setXYZ(i, x, newY, newZ);
         }
 
         // Mark the particle positions as needing an update
@@ -40,29 +49,73 @@ export const Start_Background = () => {
     }
   });
 
+  const calculateConcentration = (x: number, y: number) => {
+    // Calculate the concentration based on the ovule shape
+    const a = 2.0; // Adjust the major axis of the ovule
+    const b = 1.0; // Adjust the minor axis of the ovule
+
+    // Calculate the equation of the ovule shape
+    const equation = (x ** 2) / (a ** 2) + (y ** 2) / (b ** 2);
+
+    // Define the concentration based on the ovule shape
+    const maxConcentration = 1.0; // Adjust the maximum concentration as desired
+    const concentration = 1.0 - equation / maxConcentration;
+
+    return concentration > 0 ? concentration : 0; // Ensure the concentration is non-negative
+  };
+
   const generateParticles = () => {
-    const particleCount = 200000;
+    const particleCount = 50000;
     const particles = new THREE.BufferGeometry();
     const particlePositions = new Float32Array(particleCount * 3);
+    const horizontalDistance = 0.05; // Adjust the value as desired
+  
+    for (let i = 0; i < particleCount; i+=3) {
+      const index = i * 3;
+      let x, y, z;
 
-    for (let i = 0; i < particleCount * 3; i++) {
-      particlePositions[i] = Math.random() * 10 - 8;
+      const check = Math.random()
+
+        // Define the concentration areas
+        if (check < 0.2) {
+            // Concentrated area (ovule shape)
+            const radius = Math.random() * 0.5 + 0.5; // Adjust the radius of the ovule area
+            const angle = Math.random() * Math.PI * 2;
+      
+            x = Math.cos(angle) * radius;
+            y = Math.sin(angle) * radius;
+      
+            z = Math.random() * 0.1 - 0.05; // Adjust the range as desired
+          } else {
+            // Sparse area (outside the ovule shape)
+            x = Math.random() * 10 - 5; // Adjust the range as desired
+            y = Math.random() * 5 - 2.5; // Adjust the range as desired
+            z = Math.random() * 0.1 - 0.05; // Adjust the range as desired
+          }
+      
+        // Calculate the concentration at the particle's position
+        const concentration = calculateConcentration(x, y);
+        
+      particlePositions[index] = x*concentration//Math.random() * 16 -8;
+      particlePositions[index + 1] = y*concentration//Math.random() * 10 - 5;
+      particlePositions[index + 2] = z*concentration//Math.random() * 0.1 - 0.05; // Adjust the range as desired
     }
-
+  
     particles.setAttribute(
       'position',
       new THREE.BufferAttribute(particlePositions, 3)
     );
-
+  
     const particleMaterial = new THREE.PointsMaterial({
       color: new THREE.Color(0xffffff),
       size: 0.01,
       transparent: true,
-      opacity: 0.8,
+      opacity: 1,
     });
-
+  
     return <points name="particle" geometry={particles} material={particleMaterial} />;
   };
+  
 
   return <group ref={groupRef}>{generateParticles()}</group>;
 };
