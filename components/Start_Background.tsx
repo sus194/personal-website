442dea8e62532258e47e5f4c1e31153fe9945201
sus194 <spa194@sfu.ca>
2023-07-10 02:03:45 -0700

    the floating mesh for the start screen is complete

--- components/Start_Background.tsx
+++ components/Start_Background.tsx
@@ -22,40 +22,38 @@ export const Start_Background = () => {
 
       if (particle && particle instanceof Points) {
         const particlePositions = particle.geometry.attributes.position;
-        //const particleColors = particle.geometry.attributes.color;
+        const particleColors = particle.geometry.attributes.color;
 
         for (let i = 0; i < particlePositions.count; i++) {
             const x = particlePositions.getX(i);
             const y = particlePositions.getY(i);
             const z = particlePositions.getZ(i);
             
-            const waveOffset = Math.sin(elapsedTime+ (x* 0.2)) * 0.01;
-            const waveOffsetz = Math.sin(elapsedTime + (y* 0.5 )+(x* 0.2)) * 0.01; // Adjust the amplitude and frequency as desired
+            const waveOffset = (Math.random()*(x* 0.001) + Math.random()*(y* 0.06)) * 0.01;
+            
+            const waveOffsetz = Math.sin(elapsedTime + (y* 0.8 )+(x* 0.6)) * 0.03; // Adjust the amplitude and frequency as desired
             const direction = new THREE.Vector2(1, 1);
             let newZ = (z + direction.x *waveOffsetz); // Adjust the range as desired
+            let newX = x 
+            let newY = y
+            
 
             
-            let newX = (x +  direction.x *waveOffset);
-            let newY = (y +  waveOffset);
             
             // Calculate the darkness based on the y position
-            //const darkness = Math.max(0, (newY + 2) / 4);
+            const darkness = Math.max(0, (newZ+(Math.random()*0.01+1)) / (Math.random()*0.01+2));
 
             // Calculate the new color based on the darkness
-            //const color = new THREE.Color(0xffffff);
-            //color.lerpHSL(new THREE.Color(0x000000), darkness);
+            const color = new THREE.Color(0xffffff);
+            color.lerpHSL(new THREE.Color(0xffffff), darkness);
 
-
-            
-
-
-            particlePositions.setXYZ(i, x, y, newZ);
-            //particleColors.setXYZ(i, color.r, color.g, color.b);
+            particlePositions.setXYZ(i, newX, newY, newZ);
+            particleColors.setXYZ(i, color.r, color.g, color.b);
         }
 
         // Mark the particle positions as needing an update
         particlePositions.needsUpdate = true;
-        //particleColors.needsUpdate = true;
+        particleColors.needsUpdate = true;
       }
     }
     
@@ -65,7 +63,7 @@ export const Start_Background = () => {
 
   
   const generateParticles = () => {
-    const particleCount = 290000;
+    const particleCount = 90000;
     const particles = new THREE.BufferGeometry();
     const particlePositions = new Float32Array(particleCount * 3);
     const particleColors = new Float32Array(particleCount*3);
@@ -99,9 +97,34 @@ export const Start_Background = () => {
             z = Math.random() * 0.1 - 0.05; // Adjust the range as desired
         }
 */
-        x = Math.random() * 8 - 4; // Adjust the range as desired
-        y = Math.random() * 7 - 3.5; // Adjust the range as desired
+    if(check<0.3){
+
+        x = Math.random() * 5-2.5; // Adjust the range as desired
+        y = Math.random()*1 -2; // Adjust the range as desired
         z = Math.random() * 0.1 - 0.05; // Adjust the range as desired
+
+    }
+
+    else if(check>=0.3 && check<0.6){
+        x = Math.random() * 5  - 2.5; // Adjust the range as desired
+        y = Math.random() * 1; // Adjust the range as desired
+        z = Math.random() * 0.1 - 0.05; // Adjust the range as desired
+    }
+
+    else if(check>=0.6 && check<0.7){
+        x = Math.random() * 5  - 2.5; // Adjust the range as desired
+        y = Math.random() * 3 -1; // Adjust the range as desired
+        z = Math.random() * 0.1 - 0.05; // Adjust the range as desired
+    }
+    else{
+
+        x = Math.random() * 5 -2.5; // Adjust the range as desired
+        y = Math.random() * 1 + 2; // Adjust the range as desired
+        z = Math.random() * 0.1 - 0.05; // Adjust the range as desired
+
+        
+
+    }
 
         const color = new THREE.Color(0xffffff);
 
@@ -127,7 +150,8 @@ export const Start_Background = () => {
     const particleMaterial = new THREE.PointsMaterial({
       size: 0.001,
       transparent: true,
-      opacity: 0.9,
+      opacity: 1,
+      vertexColors: true, // Enable vertex colors
     });
   
     return <points name="particle" geometry={particles} material={particleMaterial} />;
