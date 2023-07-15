888ad4eee5c6f2c34af1ca4abfb6ecbe22606749
sus194 <spa194@sfu.ca>
2023-07-15 03:41:49 -0700

    solarobjects are finally added

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -4,15 +4,24 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { Tween,Easing } from '@tweenjs/tween.js';
 
-function Sphere({ position, radius, texture, onClick }) {
+function Sphere({ position, radius, texture, onClick, name }) {
   const meshRef = useRef<THREE.Mesh>(null);
   
-  
   return (
-    <mesh ref={meshRef} position={position}>
-      <sphereGeometry args={[radius]} />
-      <meshStandardMaterial map={texture} />
-    </mesh>
+    <group>
+      <mesh ref={meshRef} position={position}>
+        <sphereGeometry args={[radius]} />
+        <meshStandardMaterial map={texture} />
+      </mesh>
+      {name == 'satrun' && (
+        <mesh position={position} rotation={[181, 0, 0]}>
+          <ringGeometry args={[0.6, 0.9, 30, 0, 0, 6.283185307179586]} />
+          <meshStandardMaterial map={new THREE.TextureLoader().load('imgs/satrun-ring.jpeg')} side={THREE.DoubleSide} />
+        </mesh>
+      )}
+    </group>
+
+    
   );
 }
 
@@ -22,10 +31,56 @@ function Sun() {
   
   
   return (
-    <mesh ref={meshRef} position={[0, 0, 0]}>
-      <sphereGeometry args={[1]} />
-      <meshStandardMaterial map={new THREE.TextureLoader().load('imgs/sun.jpeg')} />
-    </mesh>
+    <group >
+      <mesh ref={meshRef} position={[0, 0, 0]}>
+        <sphereGeometry args={[1]} />
+        <meshStandardMaterial map={new THREE.TextureLoader().load('imgs/sun.jpeg')} />
+      </mesh>
+
+      <mesh position={[0, 0, 0]} rotation={[300, 0, 0]}>
+      <torusGeometry args={[1.5, 0.02, 64, 64]} />
+      <meshStandardMaterial color="gray" />
+      </mesh>
+
+      <mesh position={[0, 0, 0]} rotation={[300, 0, 0]}>
+      <torusGeometry args={[2.2, 0.02, 64, 64]} />
+      <meshStandardMaterial color="gray" />
+      </mesh>
+
+      <mesh position={[0, 0, 0]} rotation={[300, 0, 0]}>
+      <torusGeometry args={[2.6, 0.02, 64, 64]} />
+      <meshStandardMaterial color="gray" />
+      </mesh>
+
+      <mesh position={[0, 0, 0]} rotation={[300, 0, 0]}>
+      <torusGeometry args={[3.6, 0.02, 64, 64]} />
+      <meshStandardMaterial color="gray" />
+      </mesh>
+
+      <mesh position={[0, 0, 0]} rotation={[300, 0, 0]}>
+      <torusGeometry args={[5.1, 0.02, 64, 64]} />
+      <meshStandardMaterial color="gray" />
+      </mesh>
+
+      <mesh position={[0, 0, 0]} rotation={[300, 0, 0]}>
+      <torusGeometry args={[7.6, 0.02, 64, 64]} />
+      <meshStandardMaterial color="gray" />
+      </mesh>
+
+      <mesh position={[0, 0, 0]} rotation={[300, 0, 0]}>
+      <torusGeometry args={[11, 0.02, 64, 64]} />
+      <meshStandardMaterial color="gray" />
+      </mesh>
+
+
+      <mesh position={[0, 0, 0]} rotation={[300, 0, 0]}>
+      <torusGeometry args={[12.5, 0.02, 64, 64]} />
+      <meshStandardMaterial color="gray" />
+      </mesh>
+
+    </group>
+
+    
   );
 }
 
@@ -66,16 +121,70 @@ function RevolveGroup({ children }) {
 export default function Skills() {
   const spheres = [
     {
-      position: [2, 0, 1],
+      x:1.5,
+      z:0,
+      name:"mercury",
       radius: 0.1,
+      texture: new THREE.TextureLoader().load('imgs/mercury.png'),
+    },
+    {
+      x:2.3,
+      z:2,
+      name:"venus",
+      radius: 0.18,
+      texture: new THREE.TextureLoader().load('imgs/venus.jpeg'),
+    },
+
+    {
+      x:0,
+      z:-2.6,
+      name:"earth",
+      radius: 0.2,
       texture: new THREE.TextureLoader().load('imgs/earth.jpeg'),
     },
     {
-      position: [2, 0, 0],
-      radius: 0.3,
+      x:1,
+      z:-5,
+      name:"mars",
+      radius: 0.2,
       texture: new THREE.TextureLoader().load('imgs/mars.jpeg'),
     },
+
+    {
+      x:5,
+      z:0,
+      name:"jupiter",
+      radius: 0.7,
+      texture: new THREE.TextureLoader().load('imgs/jupiter.jpeg'),
+    },
+    {
+      x:10,
+      z:3.5,
+      name:"satrun",
+      radius: 0.46,
+      texture: new THREE.TextureLoader().load('imgs/satrun.webp'),
+    },
+
+    {
+      x:0,
+      z:-11,
+      name:"uranus",
+      radius: 0.5,
+      texture: new THREE.TextureLoader().load('imgs/uranus.jpeg'),
+    },
+    {
+      x:-5,
+      z:-17,
+      name:"neptune",
+      radius: 0.5,
+      texture: new THREE.TextureLoader().load('imgs/neptune.jpeg'),
+    },
   ];
+
+
+  const numSpheres = spheres.length;
+  
+  const angleIncrement = (2 * Math.PI) / numSpheres;
 
   return (
     <Canvas>
@@ -85,14 +194,22 @@ export default function Skills() {
       <OrbitControl />
       <Sun/>
       <RevolveGroup>
-        {spheres.map((sphere, index) => (
-          <Sphere
-            key={index}
-            position={sphere.position}
-            radius={sphere.radius}
-            texture={sphere.texture} 
-            onClick/>
-        ))}
+      {spheres.map((sphere, index) => {
+          const angle = index * angleIncrement;
+          const x = sphere.x * Math.cos(angle);
+          const z = sphere.z * Math.sin(angle);
+          const position = [x, 0, z];
+          return (
+            <Sphere
+              key={index}
+              position={position}
+              radius={sphere.radius}
+              texture={sphere.texture}
+              name = {sphere.name}
+              onClick
+            />
+          );
+        })}
       </RevolveGroup>
     </Canvas>
   );
