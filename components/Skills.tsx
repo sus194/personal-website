36c3b2b14b705ac9a2b90453581228b3607e7fb1
sus194 <spa194@sfu.ca>
2023-07-14 22:07:19 -0700

    added sun, earth and mars and also added revolution

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,25 +1,99 @@
-import React, { useEffect, useRef } from 'react';
-
+import React, { useEffect, useRef, useState } from 'react';
+import { Canvas, useThree, useFrame } from 'react-three-fiber';
 import * as THREE from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { Tween,Easing } from '@tweenjs/tween.js';
+
+function Sphere({ position, radius, texture, onClick }) {
+  const meshRef = useRef<THREE.Mesh>(null);
+  
+  
+  return (
+    <mesh ref={meshRef} position={position}>
+      <sphereGeometry args={[radius]} />
+      <meshStandardMaterial map={texture} />
+    </mesh>
+  );
+}
+
+function Sun() {
+  const meshRef = useRef<THREE.Mesh>(null);
+
+  
+  
+  return (
+    <mesh ref={meshRef} position={[0, 0, 0]}>
+      <sphereGeometry args={[1]} />
+      <meshStandardMaterial map={new THREE.TextureLoader().load('imgs/sun.jpeg')} />
+    </mesh>
+  );
+}
+
+
+
+function OrbitControl() {
+  const { camera, gl } = useThree();
+
+  useEffect(() => {
+    const controls = new OrbitControls(camera, gl.domElement);
+
+    // Optionally, customize the controls' behavior
+    controls.enableDamping = true; // Add smooth damping effect to camera movement
+    controls.enablePan = true; // Enable panning across the scene
+
+    return () => {
+      controls.dispose(); // Clean up the controls when the component unmounts
+    };
+  }, [camera, gl.domElement]);
+
+  return null;
+}
+
+
+function RevolveGroup({ children }) {
+  const groupRef = useRef<THREE.Group>(null);
+  useFrame(({ clock }) => {
+    if (groupRef.current) {
+      groupRef.current.rotation.y = clock.getElapsedTime() * 0.1; // Adjust the revolving speed as needed
+    }
+  });
+
+  return <group ref={groupRef}>{children}</group>;
+}
+
+
 
 export default function Skills() {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-  const renderer = new THREE.WebGLRenderer(
+  const spheres = [
     {
-      canvas: canvasRef.current!,
-      antialias: true,
-    }
-  );
-  renderer.setSize( window.innerWidth, window.innerHeight );
-  const geometry = new THREE.SphereGeometry( 15, 32, 16 ); 
-  const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } ); 
-  const sphere = new THREE.Mesh( geometry, material ); 
-  scene.add( sphere );
+      position: [2, 0, 1],
+      radius: 0.1,
+      texture: new THREE.TextureLoader().load('imgs/earth.jpeg'),
+    },
+    {
+      position: [2, 0, 0],
+      radius: 0.3,
+      texture: new THREE.TextureLoader().load('imgs/mars.jpeg'),
+    },
+  ];
+
   return (
-    <canvas ref={canvasRef}/>
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[5, 5, 3]} intensity={3} />
+      <pointLight position={[-3, -3, 2]} />
+      <OrbitControl />
+      <Sun/>
+      <RevolveGroup>
+        {spheres.map((sphere, index) => (
+          <Sphere
+            key={index}
+            position={sphere.position}
+            radius={sphere.radius}
+            texture={sphere.texture} 
+            onClick/>
+        ))}
+      </RevolveGroup>
+    </Canvas>
   );
 }
