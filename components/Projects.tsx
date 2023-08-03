a32805a07613bb2d0ddb73cf5a4d3e7d9f7bc1e1
sus194 <spa194@sfu.ca>
2023-08-02 23:20:15 -0700

    check

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -5,35 +5,7 @@ import 'slick-carousel/slick/slick-theme.css';
 
 export default function Projects() {
   // Sample data for projects
-  const projects = [
-    {
-      name: 'Project 1',
-      description: 'Description for Project 1',
-      images: [
-        'imgs/projects/aidiscord-bot/aibot-ai.png',
-        // Add more image URLs here
-      ],
-    },
-    {
-      name: 'Project 2',
-      description: 'Description for Project 2',
-      images: [
-        'imgs/projects/knight-run/knight-run-demo1.png',
-        
-        // Add more image URLs here
-      ],
-    },
-    // Add more projects here
-  ];
-
-  // Slick settings for the carousel
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
+  
 
   return (
     <div>
