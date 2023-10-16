2653efb1fbdc010eacbe37ea2cc7a9fbf6d17cfd
sus194 <spa194@sfu.ca>
2023-10-16 13:18:41 -0700

    last minute changes 4

--- components/VideoPlayer.tsx
+++ components/VideoPlayer.tsx
@@ -1,66 +1,52 @@
 import React, { useRef, useState } from 'react';
+
 import '@styles/video.css';
 
 const VideoPlayer = (props: any) => {
   const videoRef = useRef<any>();
   const [isPaused, setIsPaused] = useState(false);
-  const [isVideoPlain, setIsVideoPlain] = useState(true);
-  const [showPrompt, setShowPrompt] = useState(false);
-
+  const [isvideoplain, setisvideoplain] = useState(true);
+  const [show, isshow] = useState(false);
   const handleVideoClick = () => {
     if (videoRef.current) {
-      if (isPaused) {
-        videoRef.current.play(); // Resume the video
-      } else {
-        videoRef.current.pause();
-        setShowPrompt(false);
-      }
-      setIsPaused(!isPaused);
+      setIsPaused(true);
+      isshow(false)
     }
   };
 
   const handleVideoTimeUpdate = () => {
-    if (videoRef.current) {
-      if (videoRef.current.currentTime >= 3.18) {
-        videoRef.current.currentTime = 3.17;
-      }
-      if (videoRef.current.currentTime >= 2) {
-        setShowPrompt(true);
-      }
+    if (videoRef.current && videoRef.current.currentTime >= 3.18) {
+      videoRef.current.currentTime = 3.17;
+     
+    }
+    if(videoRef.current && videoRef.current.currentTime >= 2){
+        isshow(true)
     }
   };
 
-  const handleVideoEnd = () => {
-    setShowPrompt(false);
-    setIsVideoPlain(false);
+  const handleideoTimeUpdate = () => {
+    if((videoRef.current && videoRef.current.currentTime >= 8.9)){
+        setisvideoplain(false)
+    }
   };
-
   return (
     <div className='ooverlay'>
-      {props.handleCallback(isVideoPlain)}
-      {isVideoPlain && (
-        <>
-          <video
-            onClick={handleVideoClick}
-            ref={videoRef}
-            onTimeUpdate={isPaused ? handleVideoTimeUpdate : handleVideoTimeUpdate}
-            onEnded={handleVideoEnd}
-            autoPlay
-            muted
-            loop
-            className="video"
-          >
-            <source src="/video/bleach.mp4" type="video/mp4" />
-          </video>
-          {showPrompt && (
-            <div className={`contet-overlay ${showPrompt ? 'fade-in' : ''}`}>
-              <h1>Click To Continue</h1>
-            </div>
-          )}
-        </>
-      )}
+        {props.handleCallback(isvideoplain)}
+    {isvideoplain?(
+    <><video onClick={handleVideoClick} ref={videoRef} onTimeUpdate={isPaused ? handleideoTimeUpdate : handleVideoTimeUpdate} autoPlay muted loop className="video">
+                  <source src="/video/bleach.mp4" type="video/mp4" />
+              </video>
+    {show?(
+    <div className={`contet-overlay ${show ? 'fade-in' : ''}`}>
+        <h1>Click To Continue</h1>
+    </div>):(null)
+    }   
+    </>
+    
+    ):(null)
+    }
     </div>
-  );
+  )
 };
 
 export default VideoPlayer;
